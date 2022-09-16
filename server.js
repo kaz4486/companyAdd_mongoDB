2777c142d52fcbce915f86fbfe33e292ee034d5c
Kamil <kaz4486@gmail.com>
2022-09-16 22:59:44 +0200

    Add tests for endpoints

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+// connects our backend code with the database
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production') dbUri = 'url to remote db';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
+else dbUri = 'mongodb://localhost:27017/companyDB';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
@@ -20,17 +31,13 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
 
-// connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/companyDB', {
-  useNewUrlParser: true,
-});
-const db = mongoose.connection;
-
 db.once('open', () => {
   console.log('Connected to the database');
 });
 db.on('error', (err) => console.log('Error ' + err));
 
-app.listen('8000', () => {
+const server = app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
+
+module.exports = server;
